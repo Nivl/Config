ad7d03b504505efc4f7360a51018261461ef168d
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2012-06-03 19:40:15 +0200

    Improve python indentation

--- .emacs.d/hooks.el
+++ .emacs.d/hooks.el
@@ -1,6 +1,37 @@
 ;;; hooks.el ---
 
+;; python
+
+(require 'cl)
+
+(defadvice python-calculate-indentation (around continuation-with-dot)
+  "Handle continuation lines that start with a dot and try to
+line them up with a dot in the line they continue from."
+  (unless
+      (block 'found-dot
+        (save-excursion
+          (beginning-of-line)
+          (when (and (python-continuation-line-p)
+                     (looking-at "\\s-*\\."))
+            (save-restriction
+              (narrow-to-region (point)
+                                (save-excursion
+                                  (end-of-line -1)
+                                  (python-beginning-of-statement)
+                                  (point)))
+              (let ((p -1))
+                (while (/= p (point))
+                  (setq p (point))
+                  (when (looking-back "\\.")
+                    (setq ad-return-value (1- (current-column)))
+                    (return-from 'found-dot t))
+                  (ignore-errors (backward-sexp))))))))
+    ad-do-it))
+
+(ad-activate 'python-calculate-indentation)
+
 ; C/C++
+
 
 ;; Le module ctypes permet d'ajouter à la liste des types de données
 ;; C/C++ des types non reconnus par défaut. Ces types sont alors
